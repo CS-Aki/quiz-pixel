cbffa62a531899583d14237441ffea61024bebc9
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2026-04-05 17:28:35 +0800

    - Updated quiz dashboard look

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,18 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="icon" href="{{ asset('img/icon.png') }}" type="image/png">
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans&display=swap" rel="stylesheet">        <!-- Fonts -->
+        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> --}}
 
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+
+    <style>
+    body {
+        font-family: 'Pixelify Sans', monospace;
+    }
+    </style>
 
     <body>
         {{ $slot }}
